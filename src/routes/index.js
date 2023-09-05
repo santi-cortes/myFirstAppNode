10e6ec8a5ec4cfc4822e648f64d6cadd9977c777
Santi_Cortes <santya.2003.06@gmail.com>
2023-09-05 03:08:44 -0500

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+const router = Router();
+
+router.get('/', (req, res) =>
+	res.render('index', { title: 'First web', x: 30 })
+);
+
+router.get('/about', (req, res) => res.render('about', { title: 'About me' }));
+
+router.get('/contact', (req, res) =>
+	res.render('contact', { title: 'Contact me' })
+);
+
+export default router;
